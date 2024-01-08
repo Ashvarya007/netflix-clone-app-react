35d99cba6e6433db36e11ddd0fb97fba4ffca96f
Ashvarya007 <aishwaryaananth310@gmail.com>
2024-01-08 22:03:34 +0530

    favicon and redirect update

--- src/Requests.js
+++ src/Requests.js
@@ -4,8 +4,11 @@ const requests = {
     requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
     requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
     requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
-    requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=horror&page=1&include_adult=false`,
+    requestDrama: `https://api.themoviedb.org/3/discover/tv?api_key=${key}&with_genres=18`,
     requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
-  };
+    requestReality: `https://api.themoviedb.org/3/discover/tv?api_key=${key}&with_genres=10764`,
+    requestKids: `https://api.themoviedb.org/3/movie/kids?api_key=${key}&language=en-US&page=1&include_adult=false`,
+    requestActionAdventure: `https://api.themoviedb.org/3/discover/tv?api_key=${key}&with_genres=10759`
+};
 
   export default requests
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,8 +10,11 @@ const Home = () => {
         <Row rowID='1' title='UpComing' fetchURL={requests.requestUpcoming} />
         <Row rowID='2' title='Popular' fetchURL={requests.requestPopular} />
         <Row rowID='3' title='Trending' fetchURL={requests.requestTrending} />
-        <Row rowID='4' title='Top Rated' fetchURL={requests.requestTopRated} />
-        <Row rowID='5' title='Horror' fetchURL={requests.requestHorror} />
+      <Row rowID='4' title='Top Rated' fetchURL={requests.requestTopRated} />
+      <Row rowID='7' title='Action & Adventure' fetchURL={requests.requestActionAdventure} />
+        <Row rowID='5' title='Drama' fetchURL={requests.requestDrama} />
+      <Row rowID='6' title='Reality' fetchURL={requests.requestReality} />
+      
     </>
   )
 }
